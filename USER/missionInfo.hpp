16482fb654423535dc0902b593546341adeba73d
Headiii <maximiliangerber97@gmail.com>
2022-05-01 19:27:53 +0200

    Alot of fucking fixes

--- USER/missionInfo.hpp
+++ USER/missionInfo.hpp
@@ -2,7 +2,7 @@
 *   Legt allgemeine Information über die Mission fest.
 */
 
-author = "[A] Headiii, danke [A] Erik <3 ";                                               // Missionsersteller
+author = "[A] Headiii";                                               // Missionsersteller
 onLoadName = "Queens Gambit";                                                   // Name der Mission
 onLoadMission = "ALTIS BLEIBT STANDHAFT!";                                                             // Beschreibung der Mission (wird im Ladebildschirm unterhalb des Ladebildes angezeigt)
 loadScreen = "data\loadpic.paa";                                                // Ladebild
--- initplayerlocal.sqf
+++ initplayerlocal.sqf
@@ -1,3 +1,5 @@
+params ["_player", "_didJIP"];
+
 if (didJIP) then {
     [player] remoteExec ["grad_common_fnc_addJipToZeus",2,false];
 };
@@ -6,5 +8,7 @@ if (didJIP) then {
 grad_template_ratingEH = player addEventHandler ["HandleRating",{0}];
 
 if (_didJiP || didJiP) exitWith {};
-sleep 5;
-playMusic ["russia", 0];
+// sleep 5;
+// playMusic ["russia", 0];
+
+cutText ["", "BLACK FADED"];
